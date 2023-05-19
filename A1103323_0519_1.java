dcf9a79a78ae4261cb3016cb2362299b7aab9834
Cjoechen <125528558+Cjoechen@users.noreply.github.com>
2023-05-19 11:00:30 +0800

    Add files via upload

---
+++ A1103323_0519_1.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+class TimerThread extends Thread{
+    int num;
+    public TimerThread(int num){
+        this.num = num;
+    }
+    
+    public void run(){
+        try{
+            this.sleep(1000);
+        }
+        catch(InterruptedException e){
+         
+        }
+    }
+}
+
+class Dumpling{
+    static int pork = 5000;
+    static int beef = 3000;
+    static int vegetable = 1000;
+
+    int num, type, amount;
+
+    public Dumpling(int num, int type, int amount){
+        this.num = num;
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public synchronized void total(){
+        if((pork <= 0) && (beef <= 0) && (vegetable <= 0)){
+            System.out.println("所有水餃都售出完畢");
+            System.exit(0);
+        }
+        
+        System.out.println("-------剩餘總計-------");
+        System.out.println("豬肉水餃剩餘" + pork + "份");
+        System.out.println("牛肉水餃剩餘" + beef + "份");
+        System.out.println("蔬菜水餃剩餘"+ vegetable + "份");
+        System.out.println("----------------------");
+    }
+
+    public void order(){
+        if(this.type == 1){
+            if(this.amount > pork){
+                System.out.println("豬肉水餃不足");
+            } else {
+                System.out.println("第" + this.num + "位購買了豬肉水餃：" + this.amount + "顆");
+                pork = pork - this.amount;
+            }
+        } else if(this.type == 2){
+            if(this.amount > beef){
+                System.out.println("牛肉水餃不足");
+            } else {
+                System.out.println("第" + this.num + "位購買了牛肉水餃：" + this.amount + "顆");
+                beef = beef - this.amount;
+            }
+        } else if(this.type == 3){
+            if(this.amount > vegetable){
+                System.out.println("蔬菜水餃不足");
+            } else {
+                System.out.println("第" + this.num + "位購買了蔬菜水餃：" + this.amount + "顆");
+                vegetable = vegetable - this.amount;
+            }
+        }
+    }
+}
+
+public class A1103323_0519_1{
+    public static void main(String[] args) throws Exception{
+        Scanner input = new Scanner(System.in);
+        System.out.printf("請輸入顧客數目：");
+        int num = input.nextInt();
+        
+        for(int i = 1;i <= num;i++){
+            Random random = new Random();
+            for(int type = 1;type <= 3;type++){
+                int amount =(int)(Math.random()*50);
+                if(amount < 11){
+                    amount = 0;
+                }
+
+                Dumpling dumpling = new Dumpling(i, type, amount);
+                dumpling.order();
+                dumpling.total();
+            }
+            TimerThread time = new TimerThread(i);
+            time.run();
+        }
+    }
+}
